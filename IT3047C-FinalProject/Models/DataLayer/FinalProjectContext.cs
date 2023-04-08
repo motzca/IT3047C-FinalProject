407dd121ce08bb5dc7dc9919fa6c52fecea939eb
motzca <finnickodair11@gmail.com>
2023-04-08 18:37:37 -0400

    Adding context for the database

---
+++ IT3047C-FinalProject/Models/DataLayer/FinalProjectContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IT3047C_FinalProject.Models.DataLayer
+{
+    public class FinalProjectContext : DbContext 
+    {
+        public FinalProjectContext(DbContextOptions<FinalProjectContext> options) : base(options) { }
+    }
+}
