72eeddaab5583acb03fd917fb22aa4ae4daad65c
motzca <finnickodair11@gmail.com>
2023-04-08 15:11:35 -0400

    Removing unnecessary classes

--- IT3047C-FinalProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IT3047C_FinalProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
