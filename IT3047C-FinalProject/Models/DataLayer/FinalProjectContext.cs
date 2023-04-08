e7a8928128cd860d52d6bce1d7a1a638b5fe2a9e
motzca <finnickodair11@gmail.com>
2023-04-08 18:55:33 -0400

    Matching main

--- IT3047C-FinalProject/Models/DataLayer/FinalProjectContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace IT3047C_FinalProject.Models.DataLayer
-{
-    public class FinalProjectContext : DbContext 
-    {
-        public FinalProjectContext(DbContextOptions<FinalProjectContext> options) : base(options) { }
-    }
-}
