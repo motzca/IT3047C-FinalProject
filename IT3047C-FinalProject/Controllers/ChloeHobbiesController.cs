10b630096eedaab35dd5e22c9eae2ee7732a2730
motzca <finnickodair11@gmail.com>
2023-04-24 20:07:30 -0400

    Working on more controllers + html pages

---
+++ IT3047C-FinalProject/Controllers/ChloeHobbiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IT3047C_FinalProject.Models;
+
+namespace IT3047C_FinalProject.Controllers
+{
+    public class ChloeHobbiesController : Controller
+    {
+        private Repository<Chloe> chloe { get; set; }
+        
+        public IActionResult Index()
+        {
+
+            var options = new QueryOptions<Chloe>
+            {
+                OrderBy = t => t.HobbyId
+            };
+            return View(chloe.List(options));
+        }
+    }
+}
